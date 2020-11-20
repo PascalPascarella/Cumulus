07bf1ef1822803cedb74d193579f173d2fa8a186
Pascal Pascarella <pascalpascarella86@gmail.com>
2020-11-20 15:13:35 -0600

    Base logic for Login/Registration

---
+++ cumulus/users/forms.py
@@ -0,0 +1,24 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+From wtforms.validators import DataRequired,Email,EqualTo
+from wtforms import ValidationError
+from flask_wtf.file import FileField,FileAllowed
+
+From flask_login import current_user
+from cumulus.models import User
+
+class LoginForm(FlaskForm):
+    email = StringField('Email',validators=[DataRequired(),Email()])
+    password = PasswordField('Password',validators=[DataRequired()])
+    submit = SubmitField('Log In')
+
+class RegistrationForm(FlaskForm):
+    email = StringField('Email',validators=[DataRequired(),Email()])
+    username = = StringField('UserName',validators=[DataRequired()])
+    password = PasswordField('Password',validators=[DataRequired(),EqualTo(
+                                'pass_confirm',message='Passwords must match!')])
+    pass_confirm = PasswordField('Confirm Password',validators=[DataRequired()])
+
+    def check_email(self,field):
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError('Your email has already been registered!')
