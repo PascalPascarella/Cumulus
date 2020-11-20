b28a74e08d3d89bac28a049be119b7912e06e626
Pascal Pascarella <pascalpascarella86@gmail.com>
2020-11-19 23:03:15 -0600

    User properties/table columns

--- cumulus/models.py
+++ cumulus/models.py
@@ -1,5 +1,14 @@
-class User():
-    pass
+from cumulus import db
+from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import UserMixin
 
-class Posting():
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer,primary_key=True)
+    profile_image = db.Column(db.String(64),nullable=False,default='default_profile.png')
+    email = db.Column(db.String(64),unique=True,index=True)
+    username = db.Column(db.String(64),unique=True,index=True)
+
+
+class Posting(db.Model):
     pass
