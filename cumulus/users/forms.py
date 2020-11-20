a29d08de6f04bb58daa5f1b355c9abcaa864988d
Pascal Pascarella <pascalpascarella86@gmail.com>
2020-11-20 15:34:04 -0600

    Add update form, Copy check functions from registration

--- cumulus/users/forms.py
+++ cumulus/users/forms.py
@@ -14,10 +14,25 @@ class LoginForm(FlaskForm):
 
 class RegistrationForm(FlaskForm):
     email = StringField('Email',validators=[DataRequired(),Email()])
-    username = = StringField('UserName',validators=[DataRequired()])
+    username = StringField('UserName',validators=[DataRequired()])
     password = PasswordField('Password',validators=[DataRequired(),EqualTo(
                                 'pass_confirm',message='Passwords must match!')])
     pass_confirm = PasswordField('Confirm Password',validators=[DataRequired()])
+    submit = SubmitField('Register')
+
+    def check_email(self,field):
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError('Your email has already been registered!')
+    
+    def check_username(self,field):
+        if User.query.filter_by(username=field.data).first():
+            raise ValidationError('Your username has already been registered!')
+
+class UpdateUserForm(FlaskForm):
+    email = StringField('Email',validators=[DataRequired(),Email()])
+    username = StringField('UserName',validators=[DataRequired()])
+    picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg','png'])])
+    submit = SubmitField('Update')
 
     def check_email(self,field):
         if User.query.filter_by(email=field.data).first():
