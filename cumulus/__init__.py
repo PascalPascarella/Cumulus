64288717a25f5e72262435cd8b6f67524f67ac06
Pascal Pascarella <pascalpascarella86@gmail.com>
2020-11-19 21:50:23 -0600

    Add SQLAlchemy as DB and import migration capability

--- cumulus/__init__.py
+++ cumulus/__init__.py
@@ -1,6 +1,21 @@
+import os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
+
+#######################################
+### DATABASE SETUP
+#######################################
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir,'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+Migrate(app,db)
+
 
 from cumulus.core.views import core
 from cumulus.error_pages.handlers import error_pages
