257b858a09b2c68825fe8c1aa8693cbc6aeab82b
Pascal Pascarella <pascalpascarella86@gmail.com>
2020-11-21 12:00:29 -0600

    Construct login logic for view

--- cumulus/users/views.py
+++ cumulus/users/views.py
@@ -10,7 +10,7 @@ from cumulus.users.picture_handler import add_profile_pic
 ## CHECKLIST
 #############################
 
-# REGISTER
+# REGISTER -- Complete
 # LOGIN
 # LOGOUT -- Complete
 # ACCOUNT (UPDATE UserForm)
@@ -18,6 +18,7 @@ from cumulus.users.picture_handler import add_profile_pic
 
 users = Blueprint('users',__name__)
 
+#
 @users.route('/register',methods=['Get','POST'])
 def register():
     form = RegistrationForm()
@@ -29,11 +30,35 @@ def register():
         db.session.add(user)
         deb.session.commit()
         flash('Thanks for registering!')
+
         return redirect(url_for('user.login'))
 
     return render_template('register.html',form=form)
 
+#
+@users.route('/login',methods=['GET','POST'])
+def login():
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+
+        if user.check_password(form.password.data) and user is not None:
+
+            login_user(user)
+            flash('Log in success!')
+            next = request.args.get('next')
+
+            if next==None or not next[0]=='/':
+                next = url_for('core.index')
+            
+            return redirect(next)
+
+    return render_template('login.html',form=form)
+
+#
 @users.route('/logout')
 def logout():
     logout_user()
+    
     return redirect(url_for("core.index"))
