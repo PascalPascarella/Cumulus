6afab453317f9b5a833991e99891d13b89ee108a
Pascal Pascarella <pascalpascarella86@gmail.com>
2020-11-21 10:51:00 -0600

    Locate and correct spelling error during testing ln 7

--- cumulus/users/picture_handler.py
+++ cumulus/users/picture_handler.py
@@ -4,7 +4,7 @@ from flask import url_for,current_app
 
 def add_profile_pic(pic_upload,username):
     filename = pic_upload.filename
-    ext_type = fielename.split('.')[-1]
+    ext_type = filename.split('.')[-1]
     storage_filename = str(username)+'.'+ext_type
 
     filepath = os.path.join(current_app.root_path,'static\profile_pics',storage_filename)
