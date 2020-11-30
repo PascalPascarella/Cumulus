263c86c810158dd017ab66bf7f8403424c34d0f3
Pascal Pascarella <pascalpascarella86@gmail.com>
2020-11-30 10:41:46 -0600

    Rename data.db, Implement user checks for nav

--- cumulus/__init__.py
+++ cumulus/__init__.py
@@ -11,7 +11,7 @@ app.config['SECRET_KEY'] = 'mysecret'
 ### DATABASE SETUP
 #######################################
 basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir,'data.sqlite')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir,'data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
